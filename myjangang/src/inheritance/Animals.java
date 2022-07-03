0ae238de50c53b18792d8a768a7633decccce68b
surajbhi <surajbhirde96@gmail.com>
2022-07-03 09:43:37 +0530

    first commit

---
+++ myjangang/src/inheritance/Animals.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+//heirarcchical inheritance
+public class Animals {
+	public void eats() {
+		System.out.println("Eats food for living");
+	}
+
+	public void drink() {
+		System.out.println("Drinks water for living");
+
+	}
+
+}
