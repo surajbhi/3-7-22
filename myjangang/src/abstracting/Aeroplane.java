f1212d0df85e1f78b3d16869b5ee7f9e8866996d
surajbhi <surajbhirde96@gmail.com>
2022-07-03 09:44:49 +0530

    first commit

---
+++ myjangang/src/abstracting/Aeroplane.java
@@ -0,0 +1,14 @@
+package abstracting;
+
+public class Aeroplane extends Fuel {
+	public void petrol() {
+		System.out.println("Aviation petrol is being filled inside the aeroplane");
+	}
+	///////////////////////////////////////////////////////////////////////extra 
+	public void ready()//method overriding -same method name from fuel class
+	{
+		}
+	public static void stop() {
+		
+	}
+}
