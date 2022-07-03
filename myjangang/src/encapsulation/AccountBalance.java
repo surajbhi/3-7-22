1cb45bfbc23b0d8763606f4c9aecda5bd2de3e19
surajbhi <surajbhirde96@gmail.com>
2022-07-03 09:59:08 +0530

    first commit

---
+++ myjangang/src/encapsulation/AccountBalance.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class AccountBalance
+{
+private double balance;
+public void setBalance(double balance)
+{
+this.balance=this.balance+balance;	
+}
+public double getBalance()
+{
+	return balance;
+}
+}
+
